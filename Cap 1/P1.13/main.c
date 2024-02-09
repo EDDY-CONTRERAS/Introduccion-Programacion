6356ab29602609cda7115edc4bf0935e7ea2d5d8
EDDY-CONTRERAS <157148883+EDDY-CONTRERAS@users.noreply.github.com>
2024-02-09 11:39:21 -0400

    Add files via upload

---
+++ Cap 1/P1.13/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* Aplicaciones de operadores. */
+
+void main (void)
+{
+int i = 5, j = 4, k, 1, m;
+k = !i * 3 + --j * 2 - 3;
+printf ("\nEl valor de k es: %d", k);
+
+1 = ! (!i ║ 1 && Ø) && 1;
+printf ("\nEl valor de 1 es: %d", 1);
+
+m = (k = (! (12 > 1Ø)), j = (1Ø ║ Ø) && k, (! (k ║ j)));
+printf ("\nEl valor de m es: %d", m);
+}
---
+++ Cap 1/P1.2/P1.2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* Invierte datos
+El programa, al recbir como datos un conjunto de datos de entrada, invierte el orden
+de los mismos cuandon los imprime.
+
+A,B,C,y D: variables de tipo entero. */
+
+void main (void)
+
+{
+int A,B,C,D;
+printf ("Ingrse cuatro datos de tipo entero");
+scanf ("%d %d %d %d", &A, &B, &C, &B);
+printf ("\n %d %d %d %d", D, C, B, A);
+}
+
+
