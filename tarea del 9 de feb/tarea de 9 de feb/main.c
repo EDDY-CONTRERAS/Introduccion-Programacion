c4435103e3a41cef4a3cb6c0a0720a0e1665de0a
EDDY-CONTRERAS <157148883+EDDY-CONTRERAS@users.noreply.github.com>
2024-04-22 10:29:59 -0400

    Add files via upload

---
+++ tarea del 9 de feb/tarea de 9 de feb/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main(void)
+{
+    int fact [5];
+    int total = 0;
+
+    fac[0] = 1500;
+    fac[1] = 100;
+    fac[2] = 800;
+    fac[3] = 0;
+    fac[4] = 0;
+
+    for (int i = 0;1 < 5;i++)
+    {
+        total += fac[i];
+        printf("\nEl valor de fact [%d] = %d", i,fac,[i]);
+    }
+    printf ("\nEl valor total = %d", total);
+}
